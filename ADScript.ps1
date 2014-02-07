8b14035a92d358aabf8b5593c618c1f2b443164d
Matt Fuhrman <mfuhrm0@gmail.com>
2014-02-06 18:27:37 -0700

    new script

---
+++ ADScript.ps1
@@ -0,0 +1,17 @@
+Import-Module ActiveDirectoy
+
+New-ADUser –Name "Matt Fuhrman" –SamAccountName mfuhrman –DisplayName "Matt Fuhrman" -GivenName "Matt" -Surname "Fuhrman" –Enabled $true –ChangePasswordAtLogon $true -AccountPassword (ConvertTo-SecureString "Id@h0SpudT3ch5!?" -AsPlainText -force) -PassThru
+
+New-ADUser –Name "Matt Fuhrman" –SamAccountName nwhite –DisplayName "Nathan White" -GivenName "Nathan" -Surname "White" –Enabled $true –ChangePasswordAtLogon $true -AccountPassword (ConvertTo-SecureString "Id@h0SpudT3ch5!?" -AsPlainText -force) -PassThru
+
+New-ADUser –Name "Matt Fuhrman" –SamAccountName mgeorge –DisplayName "Mason George" -GivenName "Mason" -Surname "George" –Enabled $true –ChangePasswordAtLogon $true -AccountPassword (ConvertTo-SecureString "Id@h0SpudT3ch5!?" -AsPlainText -force) -PassThru
+
+New-ADUser –Name "Matt Fuhrman" –SamAccountName rcarrie –DisplayName "Ryan Carrie" -GivenName "Ryan" -Surname "Carrie" –Enabled $true –ChangePasswordAtLogon $true -AccountPassword (ConvertTo-SecureString "Id@h0SpudT3ch5!?" -AsPlainText -force) -PassThru
+
+New-ADUser –Name "Matt Fuhrman" –SamAccountName nday –DisplayName "Nic Day" -GivenName "Nic" -Surname "Day" –Enabled $true –ChangePasswordAtLogon $true -AccountPassword (ConvertTo-SecureString "Id@h0SpudT3ch5!?" -AsPlainText -force) -PassThru
+
+New-ADUser –Name "Matt Fuhrman" –SamAccountName cpinkerton –DisplayName "Chance Pinkerton" -GivenName "Chance" -Surname "Pinkerton" –Enabled $true –ChangePasswordAtLogon $true -AccountPassword (ConvertTo-SecureString "Id@h0SpudT3ch5!?" -AsPlainText -force) -PassThru
+
+New-ADUser –Name "Matt Fuhrman" –SamAccountName kholt –DisplayName "Ken Holt" -GivenName "Ken" -Surname "Holt" –Enabled $true –ChangePasswordAtLogon $true -AccountPassword (ConvertTo-SecureString "Id@h0SpudT3ch5!?" -AsPlainText -force) -PassThru
+
+New-ADUser –Name "Matt Fuhrman" –SamAccountName dburge –DisplayName "Dane Burge" -GivenName "Dane" -Surname "Burge" –Enabled $true –ChangePasswordAtLogon $true -AccountPassword (ConvertTo-SecureString "Id@h0SpudT3ch5!?" -AsPlainText -force) -PassThru
