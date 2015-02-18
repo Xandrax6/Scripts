49fb8a8f8e53eead1463110e1af028383bde3b47
Matt Fuhrman <mfuhrm0@gmail.com>
2015-02-18 11:55:14 -0700

    New Linux Scripts

--- firewall_mail.sh
+++ firewall_mail.sh
@@ -38,17 +38,21 @@ ip6tables -P FORWARD DROP
 ip6tables -P OUTPUT DROP
 
 iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp -s $1 --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp -s $1 --dport smtp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -s $1 --dport imap -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp -s $1 --dport pop3 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type 0 -d $1 -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type 8 -s $1 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -i eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A OUTPUT -p tcp -d $1 --sport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p udp --dport 53 --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -d $1 --sport smtp -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -d $1 --sport imap -m state --state ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -d $1 --sport pop3 -m state --state ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -d us.archive.ubuntu.com --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -d archive.ubuntu.com --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
